16f73f725bbbfb8a314e27f0ff0b26e0dfed35fa
TJ Barber <me@tjbarber.org>
2025-09-01 22:22:43 -0600

    built builtin abstraction and implements exit

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,92 @@
+use std::collections::HashMap;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-fn main() {
-    loop {
-        let input = prompt();
+pub enum ShellAction {
+    Continue(i32),
+    Exit(i32),
+}
 
-        if !input.is_empty() {
-            if input == "exit" {
-                break;
+pub trait BuiltinCommand {
+    fn execute(&self, args: &str) -> ShellAction;
+}
+
+pub struct Exit;
+
+impl BuiltinCommand for Exit {
+    fn execute(&self, _: &str) -> ShellAction {
+        return ShellAction::Exit(0);
+    }
+}
+
+pub struct Shell {
+    builtins: HashMap<String, Box<dyn BuiltinCommand>>,
+}
+
+impl Shell {
+    fn new() -> Self {
+        let mut builtins: HashMap<String, Box<dyn BuiltinCommand>> = HashMap::new();
+        builtins.insert("exit".into(), Box::new(Exit));
+        Shell { builtins }
+    }
+
+    fn run_loop(&self) {
+        loop {
+            let input = self.prompt();
+
+            if input.is_empty() {
+                continue;
             }
 
-            command_router(input);
+            let action = self.execute_command(input);
+
+            match action {
+                ShellAction::Continue(_) => continue,
+                ShellAction::Exit(code) => std::process::exit(code),
+            }
+        }
+    }
+
+    fn prompt(&self) -> String {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        let trimmed_input = input.trim();
+
+        return trimmed_input.to_string();
+    }
+
+    fn execute_command(&self, input: String) -> ShellAction {
+        let split_input = input.split_once(" ");
+        let name;
+        let args;
+
+        match split_input {
+            Some((first, remaining)) => {
+                // Come back to this, we need to just split the whole thing,
+                // get the first as the name,
+                // pass the rest as arguments
+                name = first.to_string();
+                args = remaining.to_string();
+            }
+            None => {
+                name = input.to_string();
+                args = String::new();
+            }
+        }
+
+        if let Some(cmd) = self.builtins.get(&name) {
+            return cmd.execute(&args);
+        } else {
+            eprintln!("{}: command not found", name);
+            return ShellAction::Continue(127);
         }
     }
 }
 
-fn prompt() -> String {
-    print!("$ ");
-    io::stdout().flush().unwrap();
-
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    let trimmed_input = input.trim();
-
-    return trimmed_input.to_string();
-}
-
-fn command_router(input: String) {
-    match input {
-        _ => println!("{}: command not found", input),
-    }
+fn main() {
+    let shell = Shell::new();
+    shell.run_loop();
 }
